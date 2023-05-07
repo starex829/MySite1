d4a0a3c4106b8bdccdb9881d65e681a09b6a4aed
Ruslan <127778950+starex829@users.noreply.github.com>
2023-05-07 22:25:10 +0300

    Add files via upload

---
+++ assets/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация и регистрация</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+<!-- Форма авторизации -->
+
+<form action="" method="">
+    <label>Логин</label>
+    <input type="text">
+    <label>Пароль</label>
+    <input type="password">
+</form>
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+if ($_SESSION ['user']) {
+    header('Location: profile.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация и регистрация</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+<!-- Форма регистрации -->
+
+<form action="vendor/signup.php" method="post" enctype="multipart/form-data">
+    <label>ФИО</label>
+    <input type="text" name="full_name" placeholder="Введите свое полное имя">
+    <label>Логин</label>
+    <input type="text" name="login" placeholder="Введите свой логин">
+    <label>Электронная почта</label>
+    <input type="email" name="email" placeholder="Введите свой адрес электронной почты">
+    <label>Аватарка профиля</label>
+    <input type="file" name="avatar">
+    <label>Пароль</label>
+    <input type="password" name="password" placeholder="Введите пароль">
+    <label>Подтверждение пароля</label>
+    <input type="password" name="password_confirm" placeholder="Подтвердите пароль">
+    <button type="submit">Войти</button>
+    <p>
+У вас уже есть аккаунт? - <a href="/index.php">авторизируйтесь!</a>
+    </p>
+    <?php
+    if ($_SESSION ['message']) {
+        echo '<p class="msg">'.$_SESSION ['message'] .'</p>';
+    }
+    unset($_SESSION ['message']);
+    ?>
+
+</form>
+
+</body>
+</html>
