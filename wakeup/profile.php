285caf106a446210cf0f0d90d5544754a6a12eb6
Ruslan <127778950+starex829@users.noreply.github.com>
2023-05-07 23:23:56 +0300

    Add files via upload

---
+++ wakeup/profile.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+if (!$_SESSION ['user']) {
+    header('Location: /');
+}
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация и регистрация</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+<!-- Профиль -->
+
+<form>
+    <img src="<?= $_SESSION ['user'] ['avatar']?>" width="200" style= "display: block; margin: 0 auto" alt="">
+    <h2 style="margin: 10px 0;"><?= $_SESSION ['user'] ['full_name'] ?></h2>
+    <a href="#"><?= $_SESSION ['user']['email']?></a>
+    <a href="vendor/logout.php" class="logout">Выход</a>
+</form>
+
+</body>
+</html>
